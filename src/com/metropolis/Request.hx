d4d4ee674dc2a4a471af097b28d6bf1d705c9563
Syed Asim Ali <s.asim.ali.se@gmail.com>
2019-05-27 02:17:30 +0500

    refactored Request logic

---
+++ src/com/metropolis/Request.hx
@@ -0,0 +1,22 @@
+package com.metropolis;
+
+import openfl.utils.Object;
+
+/**
+ * ...
+ * @author Asim
+ */
+class Request {
+	
+	private var _query:String, 
+    private var variables:Object, 
+    private var operationName:String
+	
+	public function new() 
+	{
+		
+	}
+
+
+
+}
